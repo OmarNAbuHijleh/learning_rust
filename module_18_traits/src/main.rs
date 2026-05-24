ca26e964e8ab4a2b74bf07bfac5ef330049c8fa3
Omar Abu-Hijleh <omara569@gmail.com>
2026-05-23 19:26:12 -0500

    Completed the trait bound syntax section

--- module_18_traits/src/main.rs
+++ module_18_traits/src/main.rs
@@ -27,13 +27,40 @@
 
 /*
  * Default Implementations
+ You can define the default implementation of a trait method - the implementation we write is the one that will be implemented UNLESS the type implements the method itself. This makes the default a fallback
  */
+
+/*
+ * Calling Trait Method from Another Method
+ * We can call trait methods from within other methods
+ */
+
+/*
+ * Traits for Function Parameter Constraints
+ We can use a trait to add constraints to a function parameter.
+ */
+
+/*
+ * Trait Bound Syntax
+ * The design of taking something that implements the trait as a parameter should remind us of another topic we covered - generics. They solve similar problems.
+ * With generics, we define a generic type that is a stand-in for a future concrete type and then we can use the generic within our function declaration to annotate the type of parameters and return values to be of that type
+ *
+ * The syntax we currently have is called syntactic sugar - which means it's a shortcut for a longer, alternative declaration form that uses generics. That alternative declaration form is called a trait bound.
+ * The trait bound syntax uses a generic but limits the generic type to only types that implement a trait.
+ */
+
+/*
+ * Multiple Trait Bounds
+ */
+
 use std::collections::HashMap;
 
 trait Accommodation {
     fn get_description(&self) -> String; // This says that we call this on a variable that implements the get_description method and takes a reference. It returns a string
     fn book(&mut self, name: &str, nights: u32); // A mutable reference to an instance
-
+    fn get_description2(&self) -> String { // The default implementation - this runs unless "get_description2" is implemented by the type
+        return String::from("A wonderful place to stay");
+    }
 }
 
 #[derive(Debug)]
@@ -48,6 +75,10 @@ impl Hotel {
             name: name.to_string(),
             reservations: HashMap::new(),
         }
+    }
+
+    fn summarize(&self) -> String {
+        return format!("{}: {}", self.name, self.get_description());
     }
 }
 
@@ -86,6 +117,29 @@ impl Accommodation for AirBnB {
     }
 }
 
+fn book_for_five_nights(entity: &mut impl Accommodation, guest: &str) { // Takes a reference to any type that has implements the accommodation trait!
+    println!("{}", entity.get_description());
+    entity.book(guest, 5);
+}
+
+// Reminder - the <T> is a generic type parameter. The ": Accommodation" is the trait bound, limiting us to only use types that implement the Accommodation trait.
+fn book_for_one_night<T: Accommodation>(entity: &mut T, guest: &str) {
+    println!("{}", entity.get_description());
+    entity.book(guest, 1);
+}
+
+// If we do this, both have to be of trait Accommodation but they don't have to be the same type. if we switched to using the trait bound syntax, we have to use the same type for both!!!!
+fn mix_and_max(first: &mut impl Accommodation, second: &mut impl Accommodation, guest: &str) {
+    first.book(guest, 1);
+    second.book(guest, 1);
+}
+
+// Removes the issue from above, now first and second can be of different types as long as they both implement the Accommodation trait
+fn mix_and_max2<T: Accommodation, U: Accommodation>(first: &mut T, second: &mut U, guest: &str) {
+    first.book(guest, 1);
+    second.book(guest, 1);
+}
+
 fn main() {
     println!("Implementing a trait for structs");
 
@@ -102,4 +156,24 @@ fn main() {
     println!("{:?}", hotel);
 
     println!("Default Implementations");
+    println!("{}", air_bnb.get_description2());
+    println!("{}", hotel.get_description2());
+
+    println!("Calling Trait Method from Another Method");
+    println!("{}", hotel.summarize());
+
+    println!("Traits for Funciton Parameter Constraints");
+    book_for_five_nights(&mut hotel, "Omar");
+    book_for_five_nights(&mut air_bnb, "Ali");
+    println!("{:?}", hotel);
+    println!("{:?}", air_bnb);
+
+    println!("Trait Bound Syntax");
+    book_for_one_night(&mut hotel, "Inam"); // Notice how this runs in the same manner as before, but it uses the trait bound syntax to limit the type to only those that implement the Accommodation trait
+    book_for_one_night(&mut air_bnb, "Eman");
+    println!("{:?}", hotel);
+    println!("{:?}", air_bnb);
+
+    println!("Multiple Trait Bounds")
+
 }
