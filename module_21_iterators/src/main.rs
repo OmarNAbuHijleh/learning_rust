e9574bb86db3e71d910dfc65c08ef654c58e7972
Omar Abu-Hijleh <omara569@gmail.com>
2026-07-29 22:27:58 -0500

    Starting on String Iteration

--- module_21_iterators/src/main.rs
+++ module_21_iterators/src/main.rs
@@ -32,8 +32,23 @@
 
 /*
  * The iter Method
+ * The iter method will NOT take ownership away from the original data source. It does this by creating an iterator of immutable references to the values of the collection type
  */
 
+/*
+ * The iter_mut Method
+ * This will create an iterator that yields a mutable reference to each element
+ */
+
+/*
+ * Hashmap Iteration
+ * Hashmap iteration won't inherently be ordered the same every time, because hashmaps don't have an order
+ * You'll have 2 pieces of data in every value from the hashmap
+ */
+
+/*
+ * String Iteration
+ */
 use std::collections::HashMap;
 
  fn main() {
@@ -95,4 +110,79 @@ use std::collections::HashMap;
     // NOTE: We don't have to make the iterator mutable. We can make the new owner variable mutable if desired. This happens implicitly behind the scenes in a for loop
 
     println!("The iter Method");
- }
+    let my_vector = vec![4, 8, 15, 16, 23, 42];
+    let my_iterator = my_vector.iter(); // This is redundant because of the previous note
+    for number in my_iterator { // NOTE: Behind the scenes, ownership of the iterator moved to the for loop
+        println!("{number}");
+    }
+    println!("{my_vector:?}"); // Ownership has not transferred
+
+    // NOTE: We can also achieve the same result by doing this:
+    let my_vector = vec![4, 8, 15, 16, 23, 42];
+    for number in &my_vector { // NOTE: Behind the scenes, ownership of the iterator moved to the for loop
+        println!("{number}");
+    }
+
+    let cities = vec![String::from("Chicago"), String::from("Los Angeles")];
+    for city in cities { // Doing this will transfer the ownership of the values in the vector to the "city" variable
+        println!("{city}");
+    }
+    // println!("{cities:?}"); // This won't work because ownership of the values in cities was transferred to "city"
+
+    println!("The iter_mut Method");
+    let mut flavors = [String::from("Chocolate"), String::from("Vanilla"), String::from("Strawberry")];
+    let iterator = flavors.iter_mut();
+    for flavor in iterator {
+        flavor.push_str(" Ice Cream"); // Appending to each element of the string
+    }
+    println!("{flavors:?}");
+
+    // We can also do this
+    let mut flavors = [String::from("Chocolate"), String::from("Vanilla"), String::from("Strawberry")];
+    // let iterator = flavors.iter_mut();
+    for flavor in &mut flavors { // This achieves the same effect as the above code
+        flavor.push_str(" Ice Cream"); // Appending to each element of the string
+    }
+    println!("{flavors:?}");
+    // NOTE: With some data types, we still need to use the de-reference operator (the "*") to access the value at the address if we want to modify it, because not every method (like "push_str") will do that for us
+
+    let mut school_grades = [85, 90, 72, 92];
+    for grade in &mut school_grades {
+        // grade -= 2; // This won't work and will try to perform the actionn on the reference. We have to de-reference it
+        *grade -= 2;
+    }
+    println!("{school_grades:?}");
+
+    println!("Hashmap Iteration");
+    let mut todos = HashMap::new();
+    todos.insert("Pick up groceries", false);
+    todos.insert("Study Rust", true);
+    todos.insert("Sleep", false);
+
+    for (todo_key, todo_val) in todos { // moves ownership
+        println!("Task: {todo_key}, Complete: {todo_val}");
+    }
+    // println!("{todos:?}"); // will not work because ownership moved
+
+    let mut todos = HashMap::new();
+    todos.insert("Pick up groceries", false);
+    todos.insert("Study Rust", true);
+    todos.insert("Sleep", false);
+
+    for (todo_key, todo_val) in &todos { // does not move ownership
+        println!("Task: {todo_key}, Complete: {todo_val}");
+    }
+    println!("{todos:?}"); // will work because ownership did not move
+
+    let mut todos = HashMap::new();
+    todos.insert("Pick up groceries", false);
+    todos.insert("Study Rust", true);
+    todos.insert("Sleep", false);
+
+    for (_, todo_val) in &mut todos { // does not move ownership. Can use "_" to throw away a value
+        *todo_val = true; // needed to change the value
+    }
+    println!("{todos:?}"); // will work because ownership did not move. Values successfully changed
+
+    println!("String Iteration");
+}
